0858547d4147d084626a62b4179864fbf400b8b2
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2024-05-08 16:12:26 +0700

    Hoàn thành 1 class có chức năng gửi dữ liệu đến sever hoặc đọc chuổi từ sever để khi nào cần có thể gọi dùng lại

---
+++ ThaoTacVoiDatabase/myApp/app/src/main/java/com/nhnam1710/OnlineShopAppForAndroid/MyVolleyStringRequest.java
@@ -0,0 +1,50 @@
+package com.nhnam1710.OnlineShopAppForAndroid;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyVolleyStringRequest {
+    public interface thaoTacVoiStringRequestNay{
+        public Map<String, String> guiMapLenSever(Map<String, String> param);
+        public void xuLyChuoiDocDuocTuSever(String response);
+        public void baoLoiCuaOnErrorResponse(VolleyError error);
+    }
+    public static void GuiStringRequestDenSever(String urlSever, Context context, thaoTacVoiStringRequestNay thaoTac){
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlSever,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        thaoTac.xuLyChuoiDocDuocTuSever(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        thaoTac.baoLoiCuaOnErrorResponse(error);
+                    }
+                }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<>();
+                return thaoTac.guiMapLenSever(param);
+            }
+        };
+    }
+}
+
+
